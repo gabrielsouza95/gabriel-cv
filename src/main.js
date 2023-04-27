3770c13bc368ef6fda9c425ae1cac7dbf8e3d1c9
gabrielsouza95 <alves95gabriel@gmail.com>
2023-04-27 00:19:02 -0300

    Updates

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 let pochita_mesh = new THREE.Mesh(geometry, material);
 const pochita_loader = new STLLoader();
 pochita_loader.load(
-  '3d_models/pochita_resin.stl',
+  'assets/3d_models/pochita_resin.stl',
   function(pochita_geometry) {
     pochita_mesh = new THREE.Mesh(pochita_geometry, material);
     const sizes = {"x": 10, "y": 10, "z": -30};
@@ -84,12 +84,12 @@ Array(200).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('./image_files/space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('./assets/image_files/space.jpg');
 scene.background = spaceTexture;
 
 // Avatar
 
-const portraitTexture = new THREE.TextureLoader().load('./image_files/southern_ring_nebula.png');
+const portraitTexture = new THREE.TextureLoader().load('./assets/image_files/southern_ring_nebula.png');
 
 const portrait = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: portraitTexture }));
 
@@ -97,8 +97,8 @@ scene.add(portrait);
 
 // Moon
 
-const moonTexture = new THREE.TextureLoader().load('./image_files/moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('./image_files/normal.jpg');
+const moonTexture = new THREE.TextureLoader().load('./assets/image_files/moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('./assets/image_files/normal.jpg');
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
